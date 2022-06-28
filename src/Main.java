91d215bd902264dc99d7d7a479477cc687df2e75
Аленин Андрей <aleninmailbox@gmail.com>
2022-06-28 17:22:28 +0300

    Бонус

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 100;
-        int minReplenishment = 1001;
         int replenishment = 1100;
-        int bonus1=1;
 
         int bonus;
         if (replenishment > 1000) {
